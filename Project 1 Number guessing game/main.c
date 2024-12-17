103e600caaf2681ab55c9df9d578cd56ebafdca6
MayankT10 <170873949+MayankT10@users.noreply.github.com>
2024-12-17 19:55:56 +0530

    Project 1 done

--- Project 1 Number guessing game/main.c
+++ Project 1 Number guessing game/main.c
@@ -7,7 +7,67 @@
 // Hint: Use loop & use a random number generator.
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
+
+// Using fo while loop
 int main() {
     
+    srand(time(0));
+    int num;
+    int random_number =(rand()%100)+1;
+    int guess_count =0;
+        
+        do {
+            printf("Enter a number:");
+            scanf("%d",&num);
+            if(num>random_number) {
+                printf("Lower number please\n");
+            } else if (num<random_number) {
+                printf("Higher number please\n");
+            } else {
+                printf("You guessed it right.\n");
+            }
+            guess_count++;
+        } while(num!= random_number);
+    
+    printf("You have guessed the number in %d guesses\n",guess_count);
+    return 0;
+    
 }
+
+
+
+
+// Using while loop
+// int main() {
+//     //intializes random number generator
+//     srand(time(0)); 
+//     // Generates randon number between 1 and 100
+//     int random_number = (rand() % 100) + 1;
+//     // Prints random number
+//     // printf("Random number is %d\n",random_number);
+
+//     int num;
+//     int guess_count=0;
+//     printf("Enter a number:");
+//     scanf("%d",&num);
+//     while(num>0) 
+//     {
+//     if (num==random_number) {
+//         printf("You guessed the number right");
+//         break;
+//     } else if (num<random_number) {
+//         printf("Number is lower\nGuess the number again:");
+//         scanf("%d",&num);
+//     } else if (num>random_number) {
+//         printf("Number is higher\nGuess the number again:");
+//         scanf("%d",&num);
+//     } else {
+//         printf("Enter a valid number.");
+//     }
+//     }
+//     return 0;
+
+// }
