bfb8efecb5638867f35bf261de31431766478ada
MayankT10 <170873949+MayankT10@users.noreply.github.com>
2025-01-05 19:19:25 +0530

    Chapter 7 half done

--- Chapter 7/Chapter 7 Arrays/04_quick_quiz.c
+++ Chapter 7/Chapter 7 Arrays/04_quick_quiz.c
@@ -3,6 +3,22 @@
 #include<stdio.h>
 
 int main() {
-    int mar
+    float marks[5];
+    int num =5,i;
+
+    // accepting marks of five students
+    printf("Enter the marks of 5 students\n");
+    for(i=0;i<num;i++) {
+        printf("Student %d: ",i+1);
+        scanf("%f",&marks[i]);
+    }
+
+    // printing the marks of the five students
+    printf("\nMarks of the students are: \n");
+    for(i=0;i<num;i++) {
+        printf("Student %d: %.2f\n",i+1,marks[i]);
+    }
+
     return 0;
+
 }
